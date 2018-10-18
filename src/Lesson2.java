729fb048a9c6ec788fa0bf32dfd0e9c38f7019dc
jacklucia <jackpierrelucia@gmail.com>
2018-10-18 15:37:25 -0700

    correct for loop

--- src/Lesson2.java
+++ src/Lesson2.java
@@ -1,2 +1,11 @@
 public class Lesson2 {
+
+    public static void main(String[] args){
+
+    for (int abort = 15; abort < 30; abort++){
+        System.out.println("yeetus thy feetus");
+      }
+    }
+
+
 }
