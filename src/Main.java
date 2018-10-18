508de2ff2bd1a2c9cb7862b354f2c64dc76b657b
briannapeters <brianna.peterson@student.rjuhsd.us>
2018-10-18 14:56:46 -0700

    Says hello and main thing

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args){
         System.out.println("This is the main");
+        sayHello();
+    }
+
+    public static void sayHello()
+    {
+        System.out.println("Hello");
     }
 }
