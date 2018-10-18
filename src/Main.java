af443a942279a1959dc0cffd5493b613eec7e0c4
jacklucia <jackpierrelucia@gmail.com>
2018-10-18 15:01:15 -0700

    pushing lesson 2

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
 public class Main {
     public static void main(String[] args){
+        int one = 1;
         System.out.println("This is the main");
+        if (one > 2)
         sayHello();
-    }
+        else
+            sayGoodbye();
 
+
+    }
+    public static void sayGoodbye()
+    {
+        System.out.println("Bye");
+    }
     public static void sayHello()
     {
         System.out.println("Hello");
